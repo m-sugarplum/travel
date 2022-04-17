6e1885080cde28b4243863d6246f375bce787f2f
Magdalena Sliwinska <madziakorczak7@gmail.com>
2022-04-18 01:19:56 +0200

    add desitations template with destinations.css, change styles.css to home.css

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
+})
+
+app.get('/destinations', (req, res) => {
+    res.render('destinations.ejs')
 })
 
 app.listen(3000, () => {
