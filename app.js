268a9bc2d9cb6c3b9d85a245b87dd187eb3495ba
Magdalena Sliwinska <madziakorczak7@gmail.com>
2022-04-25 21:21:00 +0200

    Create page to add new destination - web form with styling

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ const path = require('path');
 const destinationsMexico = require('./mexico-destinations.json');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// let destinationsMexico = ['San Cristóbal de las Casas', 'Parque Nacional Lagunas de Montebello', 'Lagos de Colón',
-//     'Cascada El Chiflón', 'Palenque', 'La Selva Lacandona', 'Cancún', 'Mazatlán', 'Acapulco', 'Santuario de la Mariposa Monarca',
-//     'Huasteca potosina', 'León', 'Tequila', 'El Cañón del Sumidero']
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-app.get('/destinations', (req, res) => {
+app.get('/destinations-mexico', (req, res) => {
     const destinations = destinationsMexico;
     const listOfPlaces = Object.keys(destinationsMexico);
     const numOfPlaces = listOfPlaces.length;
-
     // console.log(places.Cancun.img)
     res.render('destinations.ejs', { destinations, listOfPlaces, numOfPlaces });
 })
 
-app.get('/destinations/:name', (req, res) => {
+app.get('/destinations-mexico/new', (req, res) => {
+    res.render('new.ejs');
+})
+
+
+app.get('/destinations-mexico/:name', (req, res) => {
     const { name } = req.params;
     const data = destinationsMexico[name];
     res.render('details.ejs', { data, name })
