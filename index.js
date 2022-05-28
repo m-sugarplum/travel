05c909b66f21ff459638a5058601a109463eaaf0
Magdalena Sliwinska <madziakorczak7@gmail.com>
2022-05-29 00:41:00 +0200

    Add patch request to edit placesMexico.json (using data from /mexico/:id/edit) and display updated information in destination details page (details.ejs)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const path = require('path');
 const fs = require('fs');
 let placesMexico = require('./placesMexico.json');
-const res = require('express/lib/response');
+let placesMexicoTest = require('./placesMexicoTest.json');
+
+// const res = require('express/lib/response');
 
 let listOfPlaces = Object.keys(placesMexico);
 let numOfPlaces = listOfPlaces.length;
@@ -13,6 +16,7 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, '/public')));
 
 
@@ -27,7 +31,7 @@ app.get('/about', (req, res) => {
 
 
 app.get('/mexico', (req, res) => {
-    res.render('destinations.ejs', {
+    return res.render('destinations.ejs', {
         placesMexico,
         listOfPlaces,
         numOfPlaces
@@ -60,26 +64,64 @@ app.get('/mexico/:id', (req, res) => {
     res.render('details.ejs', { data, id });
 })
 
+
 app.get('/mexico/:id/edit', (req, res) => {
     const { id } = req.params;
-    let data = placesMexico[id];
-    let description = data.description;
-    let placeName = data.placeName;
-    let city = data.city;
-    let state = data.state;
-    let img = data.imgWide;
-    console.log(id)
-
-    res.render('update.ejs', { description, placeName, city, state, img, id })
+    let location = placesMexico[id];
+    res.render('update.ejs', { location, id })
 })
-// app.patch('/mexico/:id', (req, res) => {
-//     const { id } = req.params;
-//     let data = placesMexico[id];
-//     let description = data.description;
-//     const updatedDescription = req.body.description;
-//     description = updatedDescription;
-//     res.redirect('/mexico');
-// })
+
+
+app.patch('/mexico/:id', (req, res) => {
+    const { id } = req.params;
+    let data = placesMexico[id];
+
+    let updatedPlaceName = req.body.placeName;
+    let updatedCity = req.body.city;
+    let updatedState = req.body.state;
+    let updatedDescription = req.body.description;
+    let updatedImg = req.body.img;
+
+    if (updatedPlaceName.trim() != data.placeName) {
+        console.log(`Old place name - ${data.placeName}`);
+        data.placeName = updatedPlaceName;
+        console.log(`Updated place name - ${data.placeName}`)
+    };
+
+    if (updatedCity.trim() != data.city) {
+        console.log(`Old city name- ${data.city}`);
+        data.city = updatedCity;
+        console.log(`Updated city name - ${data.city}`)
+    };
+
+    if (updatedState.trim() != data.state) {
+        console.log(`Old state name- ${data.state}`);
+        data.state = updatedState;
+        console.log(`Updated state name - ${data.state}`)
+    };
+
+    if (updatedDescription.trim() != data.description) {
+        console.log(`Old description - ${data.description}`);
+        data.description = updatedDescription;
+        console.log(`Updated description - ${data.description}`);
+    };
+
+    if (updatedImg != data.imgWide) {
+        console.log(`Old img - ${data.imgWide}`);
+        data.imgWide = updatedImg;
+        console.log(`Updated img - ${data.imgWide}`);
+    };
+
+    fs.writeFileSync('placesMexico.json', JSON.stringify(placesMexico), err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+    placesMexico = fs.readFileSync('./placesMexico.json');
+
+    res.redirect('/mexico');
+})
 
 
 app.listen(8080, () => {
